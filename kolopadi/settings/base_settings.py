64af9c264ebb36e1ef4b2fe149bdd5237828ede4
olaifa ibukun <ibukunolaifa1984@example.com>
2023-11-08 04:11:11 +0100

    updated project settings

--- kolopadi/settings/base_settings.py
+++ kolopadi/settings/base_settings.py
@@ -25,7 +25,7 @@ from kolopadi.settings.packages.user_agent_settings import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = True
+DEBUG = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
